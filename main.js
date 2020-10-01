14a6880843dee194873dad3f0ac10c4fd08d744c
Raj kumar <glrajv369@gmail.com>
2020-10-01 13:37:07 +0530

    checkbox updated

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let slide = document.querySelectorAll('.gal-main')
-let radio = document.querySelectorAll('input[type="radio"]')
+let radio = document.querySelectorAll('.radio')
 let gal1 = document.getElementById('gal1')
 let gal2 = document.getElementById('gal2')
 let gal3 = document.getElementById('gal3')
@@ -8,7 +8,7 @@ let y = slide.length;
 
 function hide() {
 	for (var i = 0; i < y ; i++) {
-        radio[i].checked = false
+        radio[i].style.backgroundColor = '#fff'
 		slide[i].style.display = "none";
 	}
 }
@@ -16,31 +16,31 @@ function hide() {
 //starting the slide
 function startSlide(){
     hide();
-    radio[0].checked = true
+    radio[0].style.backgroundColor = 'var(--primary-blue)'
 	slide[0].style.display = "block";
 }
 startSlide()
 
 gal1.addEventListener('click', ()=>{
     hide();
-    radio[0].checked = true
+    radio[0].style.backgroundColor = 'var(--primary-blue)'
 	slide[0].style.display = 'block'
 })
 
 gal2.addEventListener('click', ()=>{
     hide();
-    radio[1].checked = true
+    radio[1].style.backgroundColor = 'var(--primary-blue)'
 	slide[1].style.display = 'block'
 })
 
 gal3.addEventListener('click', ()=>{
     hide();
-    radio[2].checked = true
+    radio[2].style.backgroundColor = 'var(--primary-blue)'
 	slide[2].style.display = 'block'
 })
 
 gal4.addEventListener('click', ()=>{
     hide();
-    radio[3].checked = true
+    radio[3].style.backgroundColor = 'var(--primary-blue)'
 	slide[3].style.display = 'block'
 })
